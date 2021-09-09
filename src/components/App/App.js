df13af600d35a2c1eef6118f834973e5a907bf21
Taras Dovbyus <79213421+dstaras@users.noreply.github.com>
2021-09-09 10:43:21 +0300

    done

---
+++ src/components/App/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import FormContact from "../FormContact/FormContact";
+import Filter from "../Filter/Filter";
+import ContactsList from "../ContactsList/ContactsList";
+
+export default function App() {
+  return (
+    <>
+      <h1>Phonebook</h1>
+
+      <FormContact />
+
+      <h2>Contants</h2>
+      <Filter />
+      <ContactsList />
+    </>
+  );
+}
---
+++ src/components/ContactsList/ContactsList.js
@@ -0,0 +1,37 @@
+import React from "react";
+import PropTypes from "prop-types";
+import ContactListItem from "./ContactListItem/ContactListItem";
+import { useSelector, useDispatch } from "react-redux";
+import contactsActions from "../../redux/actions";
+import { getVisibleContacts } from "../../redux/selectors";
+
+export default function ContactList() {
+  const contacts = useSelector(getVisibleContacts);
+  const dispatch = useDispatch();
+
+  const onDeleteContact = (id) => dispatch(contactsActions.deleteContact(id));
+
+  return (
+    <ul>
+      {contacts.map(({ id, name, number }) => (
+        <ContactListItem
+          key={id}
+          contactName={name}
+          contactNumber={number}
+          onClickDeleteContact={() => onDeleteContact(id)}
+        />
+      ))}
+    </ul>
+  );
+}
+
+ContactList.propTypes = {
+  contacts: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+      number: PropTypes.string.isRequired,
+    })
+  ),
+  onDeleteContact: PropTypes.func,
+};
